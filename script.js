4884a512bf8e7908632f44332e1217a08c571a48
student950 <dhanavadaisheta07@gmail.com>
2026-07-22 21:45:20 +0530

    Added startGame() function

--- script.js
+++ script.js
@@ -5,7 +5,8 @@ let hasBlackJack = false
 let isAlive = true
 let message = ""
 
-if (sum <= 20) {
+function startGame() {
+    if (sum <= 20) {
     message = "Do you want to draw a new card? 😊"
 } else if(sum === 21) {
     message = "Wohoo! You've got Blackjack! 🥳"
@@ -14,5 +15,6 @@ if (sum <= 20) {
     message = "You're out of the game! 😭"
     isAlive = false
 }
-
 console.log(message)
+}
+
